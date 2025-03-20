e4d413330fe79e995709f786e60046a8e4fe5fa9
arnavsingh04 <arnavsingh.sethi@gmail.com>
2025-03-20 17:24:43 +1100

    styled with detelet functionality added

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -7,22 +7,38 @@ import { TaskForm } from './TaskForm';
 export const App = () => {
   const isLoading = useSubscribe("tasks");  
   const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  const handleToggleChecked = ({ _id, isChecked }) =>
+    Meteor.callAsync("tasks.toggleChecked", { _id, isChecked });
+  const handleDelete = ({ _id }) =>
+    Meteor.callAsync("tasks.delete", { _id });
 
   if (isLoading()) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
-
-      <TaskForm />
-      
-      <ul>
-        {tasks.map((task) => (
-          <Task key={task._id} task={task} />
-        ))}
-      </ul> 
-    </div>
-  );
+      <div className="app">
+        <header>
+          <div className="app-bar">
+            <div className="app-header">
+              <h1>📝️ To Do List</h1>
+            </div>
+          </div>
+        </header>
+        <div className="main">
+          <TaskForm />
+  
+          <ul className="tasks">
+            {tasks.map((task) => (
+              <Task
+                key={task._id}
+                task={task}
+                onCheckboxClick={handleToggleChecked}
+                onDeleteClick={handleDelete}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
 };
